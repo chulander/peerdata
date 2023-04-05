e5a13e0838ded4b333b8798edabae8b39379cc13
chulander <bryan.p.chu@gmail.com>
2023-04-05 02:43:33 -0400

    added non-matching route redirect to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { Routes, Route } from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom";
 import Footer from "./containers/Footer";
 import Nav from "./containers/Nav";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import DataOwner from "./pages/DataOwner";
 
 export default function Example() {
   return (
@@ -11,6 +12,8 @@ export default function Example() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/data-owner" element={<DataOwner />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
       <Footer />
     </div>
