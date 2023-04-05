57ae1183b35fa0196937875c4d0e2ac809b31123
chulander <bryan.p.chu@gmail.com>
2023-04-05 04:17:11 -0400

    fixed banner height

--- src/containers/Banner.tsx
+++ src/containers/Banner.tsx
@@ -5,7 +5,7 @@ export default function Banner() {
     <section className="relative bg-[url(https://media.istockphoto.com/id/1093930098/vector/abstract-blockchain-network-background.jpg?s=1024x1024&w=is&k=20&c=evjOsQuUUGoCcAbSm-9tIA0cPrqgRD4nKYtmw_d5vpA=)] bg-cover bg-center bg-no-repeat">
       <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25"></div>
 
-      <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+      <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-1/2 lg:items-center lg:px-8">
         <div className="max-w-xl text-center sm:text-left">
           <h1 className="text-3xl font-extrabold sm:text-5xl">
             Let us find your
