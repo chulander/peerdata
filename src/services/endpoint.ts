6fc67cc94f2b2a30f86550326ec519a362f0bfec
chulander <bryan.p.chu@gmail.com>
2023-04-12 21:02:31 -0400

    removed env comment

--- src/services/endpoint.ts
+++ src/services/endpoint.ts
@@ -1,8 +1,4 @@
 export type endpoint = string;
-// api
-// https://us-east-1-shared-usea1-02.cdn.hygraph.com/content/clg6lt2pz0w8r01tfcx8z0tja/master
-// readonly
-// https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clg6lt2pz0w8r01tfcx8z0tja/master
 export const readOnlyContentApi: endpoint = `https://${
   import.meta.env.VITE_REGION
 }.hygraph.com/v2/${import.meta.env.VITE_PROJECT}`;
