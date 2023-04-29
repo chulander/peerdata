9cfa69a97dae27dd658b2883d20ffab56646bb65
chulander <bryan.p.chu@gmail.com>
2023-04-29 01:29:08 -0400

    fixed contact us form spacing

--- src/components/ContactUsForm.tsx
+++ src/components/ContactUsForm.tsx
@@ -7,10 +7,10 @@ import {
 export function ContactUsForm() {
   return (
     <div className="relative isolate bg-white">
-      <div className="mx-auto grid max-w-screen-2xl grid-cols-1 lg:grid-cols-2">
+      <div className="w-full grid grid-cols-1 lg:grid-cols-2 w-screen">
 
         <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
-          <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
+          <div className="mx-auto lg:mx-0">
             <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2">
               <svg
                 className="absolute inset-0 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
@@ -105,7 +105,7 @@ export function ContactUsForm() {
           method="POST"
           className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
         >
-          <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
+          <div className="mx-auto lg:mr-0">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
               <div className="sm:col-span-2">
                 <label
@@ -176,7 +176,7 @@ export function ContactUsForm() {
                 </div>
               </div>
             </div>
-            <div className="mt-8 flex justify-end">
+            <div className="mt-8 flex">
               <button
                 type="submit"
                 className="rounded-md bg-brand-blue px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-brand-blue/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-500"
