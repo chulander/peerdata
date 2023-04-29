a3f3300efe259e69a7c267e37965e10f39776dd2
chulander <bryan.p.chu@gmail.com>
2023-04-28 22:02:17 -0400

    added about PeerData hero

--- src/pages/About/queries/About.tsx
+++ src/pages/About/queries/About.tsx
@@ -1,6 +1,12 @@
+import AboutPeerData from "../../../containers/AboutPeerData";
+
 // import useQuery from "../../../hooks/useQuery";
 export function About() {
-  return <section></section>;
+  return (
+    <section>
+      <AboutPeerData />
+    </section>
+  );
 }
 
 export default About;
